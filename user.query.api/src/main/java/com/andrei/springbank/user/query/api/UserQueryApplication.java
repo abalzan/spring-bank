e8dbe8c962268ebc6cfcc2c205d29c836e706ddd
Andrei Balzan <andreibalzan@gmail.com>
2021-03-22 10:24:18 +0000

    add axon config

--- user.query.api/src/main/java/com/andrei/springbank/user/query/api/UserQueryApplication.java
+++ user.query.api/src/main/java/com/andrei/springbank/user/query/api/UserQueryApplication.java
@@ -1,9 +1,12 @@
 package com.andrei.springbank.user.query.api;
 
+import com.andrei.springbank.user.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({AxonConfig.class})
 public class UserQueryApplication {
 
     public static void main(String[] args) {
