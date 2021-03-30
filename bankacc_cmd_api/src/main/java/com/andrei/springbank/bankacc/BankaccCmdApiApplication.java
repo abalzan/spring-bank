b73be3194955ee87362910a000ce640d8e4201e2
Andrei Balzan <andreibalzan@gmail.com>
2021-03-30 08:16:37 +0100

    bank project setup

--- bankacc_cmd_api/src/main/java/com/andrei/springbank/bankacc/BankaccCmdApiApplication.java
+++ bankacc_cmd_api/src/main/java/com/andrei/springbank/bankacc/BankaccCmdApiApplication.java
@@ -1,9 +1,18 @@
 package com.andrei.springbank.bankacc;
 
+import com.andrei.springbank.bankacc.config.AxonConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Import(AxonConfig.class)
 public class BankaccCmdApiApplication {
 
 	public static void main(String[] args) {
--- bankacc_query_api/src/main/java/com/andrei/springbank/bankacc/BankaccQueryApiApplication.java
+++ bankacc_query_api/src/main/java/com/andrei/springbank/bankacc/BankaccQueryApiApplication.java
@@ -1,9 +1,17 @@
 package com.andrei.springbank.bankacc;
 
+import com.andrei.springbank.bankacc.config.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Import(AxonConfig.class)
 public class BankaccQueryApiApplication {
 
 	public static void main(String[] args) {
